c6ffb7b546eca54e52e9ff217646b549b69f1c27
Ben Holloway <github@bholloway.com>
2017-12-05 08:32:50 +1100

    draft server with auth

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-var http = require('http');
+// https://auth0.com/blog/hapijs-authentication-secure-your-api-with-json-web-tokens/
+'use strict';
 
-var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
-  response.writeHead(200);
-  response.end('Hello World!');
-};
-var www = http.createServer(handleRequest);
-www.listen(8080);
+const {promisify} = require('util');
+const Hapi = require('hapi');
+const glob = require('glob');
+
+const setupAuth = require('./lib/auth');
+
+const PORT = 8080;
+
+process.on('unhandledException', (error) => console.error(error));
+process.on('unhandledRejection', (error) => console.error(error));
+
+Promise.resolve()
+  .then(() => {
+    const server = new Hapi.Server();
+    server.connection({port: PORT});
+    return {server};
+  })
+  .then(setupAuth)
+  .then((context) => {
+    const {server} = context;
+    return promisify(glob)('/lib/routes/**/*.js', {root: __dirname, nodir: true})
+      .then(filenames => {
+        filenames.forEach(filename => {
+          console.log(`add route ${filename}`);
+          [].concat(require(filename)(context))
+            .forEach(obj => server.route(obj));
+        });
+        return context;
+      });
+  })
+  .then(({server}) => {
+    console.info(`starting http://localhost:${PORT}`);
+    server.start();
+  })
+  .catch(exception => {
+    console.error(exception);
+    process.exit(1);
+  });
